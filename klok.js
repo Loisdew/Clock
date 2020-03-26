b24588f7a3fb7e94fe33ec6122921a4c0e07a1da
Loisdew <61747213+Loisdew@users.noreply.github.com>
2020-03-26 21:19:01 +0100

    Add files via upload

---
+++ klok.js
@@ -0,0 +1,46 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    
+
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+
+
+    var mainHeading = document.getElementById('mainHeading');
+
+    mainHeading.classList.add('changesSize');
+
+
+    if (h > "10") {
+    //document.body.style.backgroundColor = "blue";
+    document.body.style.backgroundImage = "url(day.jpg";
+    }
+
+    else if(h < "20") {
+    document.body.style.backgroundImage = "url('starry.jpg')";
+    }
+
+    else {
+        document.body.style.backgroundColor = "black";
+    }
+
+    
+}
+
+showTime();
+
+
+
+
+
